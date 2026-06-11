be9611bc865c83b75e22453743b00f7786051ef9
Jalison-MTF06 <thehzinmtf21@gmail.com>
2026-06-10 23:16:49 -0300

    Adicionei suporte a imagens nos produtos

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, re_path
 from django.conf import settings
 from django.views.static import serve
 from core import views
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -19,3 +21,5 @@ urlpatterns = [
 
 handler404 = 'core.views.page_not_found_view'
 handler500 = 'core.views.internal_server_error_view' 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
